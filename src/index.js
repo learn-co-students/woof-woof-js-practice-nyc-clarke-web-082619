75d1bce97249f2b931b3a33e169e0e62b00eaafc
jw20191n <clairejw1015@gmail.com>
2020-11-03 20:43:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
-const container = document.getElementById('dog-bar');
+const dogsContainer = document.getElementById('dog-bar');
 const infoContainer = document.getElementById('dog-info');
-let buttonText;
 let currentDog;
+let filterOn = false;
 
-function fetchDogs(){
+function fetchDogs(callback){
     fetch('http://localhost:3000/pups')
-    .then(response => response.json())
-    .then(data => {
-        printDogs(data);
-    });
+    .then( response => response.json() )
+    .then( data => callback(data) );
 }
 
-function printDogs(data){
-    data.forEach(dog => {
+function printDogNames(dogs){
+    dogs.forEach(dog => {
         let newSpan = document.createElement('span');
+        newSpan.setAttribute('data-id', dog.id);
         newSpan.innerText = dog.name;
-        container.appendChild(newSpan);
-    });
+        dogsContainer.appendChild(newSpan);
+    })
 }
+
+function fetchSingleDog(id){
+    fetch(`http://localhost:3000/pups/${id}`)
+    .then(resp => resp.json())
+    .then(data => {
+        currentDog = data;
+        showDogInfo(data);
+    })
+}
+
 
 function showDogInfo(dog){
     infoContainer.innerHTML = '';
     let newDiv = document.createElement('div');
-    newDiv.setAttribute('data-id', dog.id);
+    let buttonText;
+    
     if (dog.isGoodDog){
         buttonText = 'Good Dog!';
     }else{
         buttonText = 'Bad Dog!';
     }
+
     newDiv.innerHTML = `
         <img src='${dog.image}'>
         <h2>${dog.name}</h2>
@@ -36,64 +47,70 @@ function showDogInfo(dog){
     infoContainer.appendChild(newDiv);
 }
 
-function changeGoodDogStatus(dog){
-    console.log(dog.isGoodDog)
-    if(dog.isGoodDog){
-        dog.isGoodDog = false;
-    }else{
-        dog.isGoodDog = true;
+
+function changeGoodDogStatus(){
+    currentDog.isGoodDog = currentDog.isGoodDog ? false : true;
+
+    let configObj = {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            isGoodDog: currentDog.isGoodDog
+        })
     }
 
-    console.log(dog)
+    fetch(`http://localhost:3000/pups/${currentDog.id}`,configObj)
+    .then(resp => resp.json())
+    .then(data => {
+        showDogInfo(data);
+        if(filterOn){
+            fetchDogs(filterGoodDogs);
+        }
+    })
+}
 
-    // let configObj = {
-    //     method: "PATCH",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         Accept: "application/json"
-    //     },
-    //     body: JSON.stringify({
-    //         isGoodDog: dog.isGoodDog
-    //     })
-    // }
 
-    // fetch(`http://localhost:3000/pups/${dog.id}`,configObj)
-
+function filterGoodDogs(dogs){
+    let goodDogs = dogs.filter(dog => dog.isGoodDog);
+    dogsContainer.innerHTML = '';
+    printDogNames(goodDogs);
 }
 
 
 //when window loaded, fetch the db.json file and print the dogs in #dog-bar
 document.addEventListener('DOMContentLoaded', (event)=>{
-    fetchDogs();
+    console.log('page loaded');
+    fetchDogs(printDogNames);
 });
 
 
-//when click on a specific dog name in the #dog-bar container, display its info
-container.addEventListener('click', (event)=>{
+document.addEventListener('click', (event)=>{
+    //when click on a specific dog name in the #dog-bar container, display its info
     if (event.target.tagName === 'SPAN'){
-        fetch('http://localhost:3000/pups')
-        .then(response => response.json())
-        .then(data => {
-            let dog = data.find(function(dog){
-                return dog.name === event.target.innerText
-            })
-            showDogInfo(dog);
-            currentDog = dog;
-        });
+        fetchSingleDog(event.target.dataset.id);
     }
-});
 
-
-//when click on the good/bad dog button, toggle the button text and send fetch request 
-//to change the db.json file 
-infoContainer.addEventListener('click', (event)=>{
+    //when click on the good/bad dog button, toggle the button text and send fetch request 
+    //to change the db.json file 
     if(event.target.id === 'isGoodDog'){
-        let button = event.target;
-        if(currentDog.isGoodDog){
-            button.textContent = "Bad Dog!";
+        changeGoodDogStatus();
+    }
+
+    //filter
+    if(event.target.id === 'good-dog-filter'){
+        console.log(filterOn);
+        if(!filterOn){
+            filterOn = true;
+            event.target.innerText = 'Filter good dogs: ON';
+            fetchDogs(filterGoodDogs);
         }else{
-            button.textContent = "Good Dog!";
+            filterOn = false;
+            event.target.innerText = 'Filter good dogs: OFF';
+            fetchDogs(printDogNames);
         }
-        changeGoodDogStatus(currentDog);
     }
 });
+
